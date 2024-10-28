008972b044fb21c8a482f783f183f7b188da0244
A22GabrielMV <a22gabrielmv@iessanclemente.net>
2024-10-28 11:52:46 +0100

    e

--- UD04_1_Inbox/app/src/main/java/com/example/ud04_1_inbox/MainActivity.kt
+++ UD04_1_Inbox/app/src/main/java/com/example/ud04_1_inbox/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,8 +45,11 @@ class MainActivity : AppCompatActivity() {
 
         val bottomBar = findViewById<BottomNavigationView>(R.id.botton_navigation)
         bottomBar.setupWithNavController(navController)
+        //wafaw
 
-        val bottomLat = findViewById<BottomNavigationView>(R.id.botton_navigation)
+
+        val view = findViewById<NavigationView>(R.id.nav_side)
+        view.setupWithNavController(navController)
 
     }
 
