c720a3695301553924544ac5e57e890656881813
A22GabrielMV <a22gabrielmv@iessanclemente.net>
2024-10-28 11:08:44 +0100

    e

---
+++ 1_11_Inbox/app/src/main/java/com/example/a1_11_inbox/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.a1_11_inbox
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutMain)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbar=findViewById<MaterialToolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        val nav =supportFragmentManager.findFragmentById(R.id.contenedorFragment) as NavHostFragment
+        val navController=nav.navController
+        toolbar.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val nav= findNavController(R.id.contenedorFragment)
+
+        NavigationUI.onNavDestinationSelected(item, nav)
+
+        return super.onOptionsItemSelected(item)
+    }
+
+}
